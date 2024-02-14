bb30772edb4a6eac376f5b4087352e2f4d79e922
RodrigoNasct <rodrigonasct13@gmail.com>
2024-02-14 13:55:15 -0300

    interface creation

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,21 @@
+import logo from './assets/logo-nlw.svg'
+
 export function App() {
-
   return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="nlw-expert logo" />
 
-    <h1 className="text-3xl font-bold underline">
-      Hello world! xD
-    </h1>
+      <form className='w-full' action="">
+        <input
+          type="text"
+          placeholder='Busque suas notas'
+          className='w-full bg-transparent text-3xl font-semibold outline-none tracking-tight placeholder:text-slate-500 '
+        />
+      </form>
 
+      <div className='h-px bg-slate-700'></div>
+    </div>
   )
-
-  
 }
 
 
