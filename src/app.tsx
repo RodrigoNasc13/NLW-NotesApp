2b0f7185ec44c962f4621c50d1d3e556c8ef5a79
RodrigoNasct <rodrigonasct13@gmail.com>
2024-02-14 18:42:05 -0300

    Notes control states

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import logo from './assets/logo-nlw.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note = {
-  date: new Date(),
-  content: ''
+interface Note {
+  id: string
+  date: Date
+  content: string
 }
 
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated (content: string) {
+    const newNote =  {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="nlw-expert logo" />
@@ -23,8 +37,11 @@ export function App() {
       <div className='h-px bg-slate-700'></div>
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
-        <NewNoteCard />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
